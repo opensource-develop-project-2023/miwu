61ac4a6beba7245cacac2bf199f831f7026bdd7a
Lunarian0928 <74519471+Lunarian0928@users.noreply.github.com>
2023-09-21 20:20:20 +0900

    utf-8 형식으로 파일을 읽도록 설정함

--- demo/src/main/java/com/miwu/demo/controller/CsvController.java
+++ demo/src/main/java/com/miwu/demo/controller/CsvController.java
@@ -6,27 +6,26 @@ import java.util.ArrayList;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
+import java.nio.charset.Charset;
 
 public class CsvController {
-    public static ArrayList <ArrayList> getCsv() throws CsvValidationException, IOException {
-        CSVReader csvReader = new CSVReader(new FileReader("tour.csv"));
+    public static ArrayList<ArrayList> getCsv() throws CsvValidationException, IOException {
+        CSVReader csvReader = new CSVReader(new FileReader("tour.csv", Charset.forName("utf-8")));
         String[] line;
-        ArrayList <ArrayList> csvInfo = new ArrayList<ArrayList>();
+        ArrayList<ArrayList> csvInfo = new ArrayList<ArrayList>();
         while ((line = csvReader.readNext()) != null) {
             ArrayList<String> tmp = new ArrayList<>();
-            for(int i = 0; i < line.length; i++) {
+            for (int i = 0; i < line.length; i++) {
                 tmp.add(line[i]);
-                }
+            }
             csvInfo.add(tmp);
         }
 
-        
         // 저장된 값 확인을 위한 출력
-        for(int i = 0; i < csvInfo.size();i++){
+        for (int i = 0; i < csvInfo.size(); i++) {
             System.out.println(csvInfo.get(i));
-        } 
-        
-         
+        }
+
         return csvInfo;
     }
 }
