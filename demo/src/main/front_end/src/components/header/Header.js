851624a4cb53be9bce4cd2595f3929ba5e065380
seon <koo0685@naver.com>
2023-10-02 15:43:13 +0900

    header footer main banner

--- demo/src/main/front_end/src/components/header/Header.js
+++ demo/src/main/front_end/src/components/header/Header.js
@@ -1,11 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { FaSearch} from "react-icons/fa";
 
-const Header = () => {
+
+function Header() {
     return (
-        <header>
-            <h1 id="logo">Miwu</h1>
-        </header>
-    );    
+       
+        <div className="header">
+            <div className="header__left">
+             
+            <h1 id="logo">미우</h1>
+           
+           </div>
+           <div className="header__center">
+                
+           <br></br>
+           <br></br>
+                    <div className="menu-item  menu-item--activ">홈</div>
+                    <div className="menu-item  menu-item--activ">테마</div>
+                    <div className="menu-item  menu-item--activ">지역</div>
+                    </div>
+                    <div className="header__input">
+                  
+                    
+                    <input type="text" placeholder="어디로, 어떤 여행을 떠날 예정인가요?"/>
+               
+                    
+                    </div>
+      
+                </div>
+            
+               
+               
+        
+
+    );
+    
 };
 
 export default Header;
