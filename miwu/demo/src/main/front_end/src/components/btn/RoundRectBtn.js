71c0d949ab1faf2dc9165e9272755733913f2578
Lunarian0928 <thanks092834@gmail.com>
2023-10-04 18:30:38 +0900

    git-test

--- miwu/demo/src/main/front_end/src/components/btn/RoundRectBtn.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-function RoundRectBtn(props) {
-    return (
-        <button type="button" className="btn btn-primary">{props.label}</button>
-    );    
-};
-
-export default RoundRectBtn;
