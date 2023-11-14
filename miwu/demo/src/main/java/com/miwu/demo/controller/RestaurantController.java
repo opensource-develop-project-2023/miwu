c1b5e3f1aa9057c96f17d12c44ca5d344cc61f35
jongyoung3 <jongyong5645@gmail.com>
2023-11-14 18:34:29 +0900

    RestaurantController import수정

--- miwu/demo/src/main/java/com/miwu/demo/controller/RestaurantController.java
+++ miwu/demo/src/main/java/com/miwu/demo/controller/RestaurantController.java
@@ -2,14 +2,14 @@ package com.miwu.demo.controller;
 
 // RequiredArgsConstructor
 import lombok.RequiredArgsConstructor;
+
 // RestController
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+
 // GetMapping
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
 
