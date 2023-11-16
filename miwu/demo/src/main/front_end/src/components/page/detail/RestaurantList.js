5c9da4a1fcb49e3bb6d22d164a6f8f1f91e9a1fa
[Lunarian0928] <[thanks092834@gmail.com]>
2023-11-16 21:36:21 +0900

    push before register return update

--- miwu/demo/src/main/front_end/src/components/page/detail/RestaurantList.js
+++ miwu/demo/src/main/front_end/src/components/page/detail/RestaurantList.js
@@ -9,25 +9,29 @@ const RestaurantList = () => {
   }, []);
 
   const fetchData = async () => {
+    console.log("hello");
     try {
-      const response = await axios.get('api/restaurant');
+      const response = await axios.get('/api/restaurant/부산광역시');
+      console.log(response);
       setRestaurants(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
+  
+  const restaurantData = restaurants.map((restaurant, index) => (
+    <div key={index}>
+      <p>Name: {restaurant.name}</p>
+      <p>Address: {restaurant.adress_last}</p>
+      <p>Category: {restaurant.category}</p>
+      <p>Image URL: {restaurant.imgUrl}</p>
+      <hr />
+    </div>
+  ))
 
   return (
-    <div>
-      {restaurants.map((restaurant, index) => (
-        <div key={index}>
-          <p>Name: {restaurant.name}</p>
-          <p>Address: {restaurant.adress_last}</p>
-          <p>Category: {restaurant.category}</p>
-          <p>Image URL: {restaurant.imgUrl}</p>
-          <hr />
-        </div>
-      ))}
+    <div className="restaurant">
+      {restaurantData}
     </div>
   );
 };
